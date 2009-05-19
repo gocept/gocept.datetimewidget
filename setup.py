f6c44d868d95cf9a785d0ed60bfaaeabc006cd6a
Daniel Havlik <dh@gocept.com>
2009-05-19 10:03:08 +0000

    initial revision

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='gocept.datetimewidget',
+      version=version,
+      description="Integration package for zc.datetimewidget whithout using zc.resourcelibrary",
+      long_description=open("CHANGES.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='date datetime widget zope2 zope3 zope.formlib',
+      author='Daniel Havlik',
+      author_email='dh@gocept.com',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gocept'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zc.datetimewidget',
+          'zope.app.publisher'
+      ],
+      )
